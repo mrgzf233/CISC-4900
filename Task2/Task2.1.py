26dea04de1a316306d592f133a58a7c2d221b2a0
Solo <jameskwan2013@gmail.com>
2024-10-14 23:07:38 -0400

    Add files via upload
    Replaced the combined_ka_list with all_dict, since the json files that were being loaded in weren't going to the intended output file.

--- Task2/Task2.1.py
+++ Task2/Task2.1.py
@@ -1,25 +1,26 @@
 import json
 
-# List to combine all the knowledge areas
-combined_ka_list = []
+# option command l for reformat
+
 
 # Input list for the json files
 ka_list = [
     "cs2013_web_final_sf.json",
     "cs2013_web_final_pl.json",
-    # manually add the rest of the json files here then use a for loop??
+    # manually add the rest of the json files here
 ]
 
-all_dict = {}
-all_dict = {"list_kas": []}
+all_dict = {
+    "curriculum": "cs2013",
+    "knowledge-areas": []
+}
+
 for json_file in ka_list:
     with open(json_file, "r") as f:
         data = json.load(f)
-    all_dict["list_ka"].append(data)
+    all_dict["knowledge-areas"].append(data)  # Should I use extend instead of append?
 
-#dictionary and list
-# need to add code here to format the output
-
-
+    # Dictionary and List, dumps everything into the new JSON file.
 with open("combined_ka.json", "w") as f:
-    json.dump(combined_ka_list, f, indent=2)
+    json.dump(all_dict, f, indent=2)
+
