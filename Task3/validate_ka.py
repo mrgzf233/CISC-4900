aee7880a9a404b13ee42b6dd04fba0d9a4d8dfb7
Solo <16392777+mrgzf233@users.noreply.github.com>
2024-10-29 19:23:54 -0400

    Validate_ka.py

---
+++ Task3/validate_ka.py
@@ -0,0 +1,14 @@
+# Option + Command + L for reformat on mac
+
+import os
+import argparse
+import csv
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-- input-file",
+                    required=True
+                    )
+parser.parse_args()
+
+
