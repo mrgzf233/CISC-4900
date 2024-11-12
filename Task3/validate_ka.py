bd1528df0bd98db79a4a720b53530cb9bf3a2a40
Solo <16392777+mrgzf233@users.noreply.github.com>
2024-11-12 01:23:21 -0500

    Validate_ka.py
    Added an error check where the end page should be greater than the begin page.

--- Task3/validate_ka.py
+++ Task3/validate_ka.py
@@ -3,7 +3,6 @@ import csv
 
 # Simple version of validate csv, not as many error checks. Only checks column name and page continuity.
 # python validate_ka.py --input-file cs2013_ka_meta.csv
-
 parser = argparse.ArgumentParser()
 parser.add_argument(
     "--input-file",
@@ -28,17 +27,26 @@ def validate_csv_content(file_path):
             print("Error: Missing required columns.")
             return False
 
+        # Check that each page_end is greater than the corresponding page_begin for every row
+        # Also makes sure that the end page can't be the same as begin page.
+        for i in range(len(rows)):
+            current_begin = int(rows[i]['page_begin'])
+            current_end = int(rows[i]['page_end'])
+
+            if current_end <= current_begin:
+                print("Page Error")
+                return False
+
         # Check page continuity, ex: end page for ch1 is 65, begin page for ch2 should be 65+1=66
         for i in range(len(rows) - 1):
             current_end = int(rows[i]['page_end'])
             next_begin = int(rows[i + 1]['page_begin'])
 
-            # false if pages aren't connected.
             if current_end + 1 != next_begin:
                 print("Page is not matching!")
                 return False
 
-    print("CSV content is valid.")
+    print("CSV file is valid.")
     return True
 
 
